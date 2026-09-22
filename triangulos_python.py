238438be9fd46480e7dacee6abe85f30d0f78c59
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2026-09-22 09:21:01 -0500

    feat: Añadir mensaje de instrucción antes de solicitar continuar en el programa de triángulos

--- triangulos_python.py
+++ triangulos_python.py
@@ -58,8 +58,8 @@ if __name__ == "__main__":
 
         # Preguntar al usuario si desea continuar
         # Unicamente se aceptará 's' para continuar, cualquier otra entrada terminará el programa
-        continuar = input("¿Desea ingresar otro triángulo? (s/n): ").strip().lower()
         print("Unicamente se aceptará 's' para continuar, cualquier otra entrada terminará el programa.")
+        continuar = input("¿Desea ingresar otro triángulo? (s/n): ").strip().lower()
         if continuar != 's':
             print("Gracias por usar el programa. ¡Hasta luego!")
             break
